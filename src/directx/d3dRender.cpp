ca9259febd5a87267621dc3320e6c532cd89bdc7
liuzh <liuzh@300624.cn>
2023-01-04 16:05:55 +0800

    增加d3dRender接口

--- src/directx/d3dRender.cpp
+++ src/directx/d3dRender.cpp
@@ -5,14 +5,35 @@
 D3DRender::D3DRender(HINSTANCE hInstance, const std::wstring& windowName, int initWidth, int initHeight)
   :D3DApp(hInstance, windowName, initWidth, initHeight), m_CBuffer(){}
 
-  D3DRender::~D3DRender{}
+D3DRender::~D3DRender{}
 
-  bool D3DRender::Init(){
-    if(!D3DApp::Init()) return false;
-    
-    if(!InitEffect()) return false;
+bool D3DRender::Init(){
+  if(!D3DApp::Init()) return false;
+  
+  if(!InitEffect()) return false;
 
-    if(!InitResource()) return false;
+  if(!InitResource()) return false;
 
-    return true;
-  }
+  return true;
+}
+
+void D3DRender::OnResize(){
+  D3DRender::OnResize();
+}
+
+void D3DRender::UpdateScene(){
+
+}
+
+void D3DRender::DrawScene(){
+
+}
+
+bool D3DRender::InitEffect(){ //blend shader
+
+}
+
+bool D3DRender::InitResource(){
+  
+}
+
